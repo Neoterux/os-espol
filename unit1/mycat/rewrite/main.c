4ce1459b3ce973ec4fd10dfaadbcf1295ebd3b30
neoterux <labfernandez2014@gmail.com>
2023-05-25 16:26:50 -0500

    Finalizado ejercicio

--- unit1/mycat/rewrite/main.c
+++ unit1/mycat/rewrite/main.c
@@ -1,18 +1,26 @@
 #include <stddef.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
 
 #define STDOUT 0
+#define STDIN 1
 #define BUFF_SIZE 256
 typedef char* string;
 
+/**
+* Write the given string into a file by it's file
+* decriptor.
+*/
 void printd(int descriptor, string message) {
     if (message == NULL) return;
-    char buffer[BUFF_SIZE];
+    // char buffer[BUFF_SIZE];
     char c;
     string pointer = message;
-    size_t bufferpoint = 0;
-    while((c = *pointer++) != 0) bufferpoint++;
-    write(descriptor, message, bufferpoint);
+    size_t bufflen = 0;
+    while((c = *pointer++)) bufflen++;
+    write(descriptor, message, bufflen);
     // while (bufferpoint < BUFF_SIZE && (c = *message++) != 0) {
     //     buffer[bufferpoint++] = c;
     // }
@@ -25,15 +33,22 @@ void print(string message) {
     printd(STDOUT, message);
 }
 
-void readline(int fd, string out, size_t buffsize) {
-
-}
-
-
-int main(int argc, char **args) {
+int main(int argc, char* argv[]) {
     if (argc > 2) {
         print("Use: mycat namefile.\n");
         return 0;
     }
-    print("Hola mundo");
+    int inputfd = STDIN;
+    if (argc != 1 && (inputfd = open(argv[1], O_RDONLY)) < 0) {
+        perror("An error ocurred while open file");
+        return errno;
+    }
+    int readres;
+    char filebuffer[BUFF_SIZE];
+    while ((readres = read(inputfd, filebuffer, BUFF_SIZE - 1))) {
+        // Ensure this is a zero terminated string (prevent buffer overflow)
+        filebuffer[readres] = 0;
+        print(filebuffer);
+    }
+    return 0;
 }
