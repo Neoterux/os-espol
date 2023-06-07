219ca0da2f5b55b5b42feccdf6580b3124b9362e
neoterux <labfernandez2014@gmail.com>
2023-06-07 08:09:27 -0500

    More advancements

--- unit2/u2-pa2/time.c
+++ unit2/u2-pa2/time.c
@@ -3,13 +3,16 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/time.h>
+#include <sys/wait.h>
 
 #define EXIT_BAD_PROCESS -2
 #define EXIT_PROC_ERROR 3
 #define EXIT_NOARGS -1
 
-inline void* next_address(void *pointer, size_t size) {
-    return pointer + size;
+int run_executable(char *cmd, char **argv) {
+    int result = execvp(cmd, argv);
+    printf("The result of executable was: %d\n", result);
+    return result;
 }
 
 int main(int argc, char *argv[]) {
@@ -17,14 +20,16 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "No executable provided\n");
         exit(EXIT_NOARGS);
     }
-    struct timeval start_time;
-    gettimeofday(&start_time, NULL);
+    struct timeval sys_start_time;
+    gettimeofday(&sys_start_time, NULL);
     pid_t exec_pid = fork();
     // Start the background process
     if (exec_pid == 0) {
-        char* executable = argv[1];
-        int result = execv(executable, argv);
-        if (result == -1) {
+        char* exec = argv[1];
+        char** nargv = argv + 1;
+        
+        //int result = execvp(executable, argv);
+        if (run_executable(exec, nargv) == -1) {
             perror("An error ocurred while executing command: ");
             exit(EXIT_PROC_ERROR);
         }
@@ -33,7 +38,11 @@ int main(int argc, char *argv[]) {
         perror("Couldn't start process: ");
         exit(EXIT_BAD_PROCESS);
     }
+    int status;
+    wait(&status);
+    printf("The status of the child: %d\n", status);
 
-    struct timeval end_time;
-    gettimeofday(&end_time, NULL);
+    struct timeval sys_end_time;
+    gettimeofday(&sys_end_time, NULL);
+    printf("System time: %ldms\n", sys_end_time.tv_usec - sys_start_time.tv_usec);
 }
